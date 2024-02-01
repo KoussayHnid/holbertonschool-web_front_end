63258ae3a586967b5bd344bc1cd109e11986884f
koussay557 <henidkousay@gmail.com>
2024-02-01 15:26:35 +0100

    Add welcomeMessage function and closure

---
+++ Javascript_advanced/2-function_me.js
@@ -0,0 +1,11 @@
+function welcomeMessage(fullName) {
+    return function() {
+      alert("Welcome " + fullName);
+    };
+  }
+  var guillaume = welcomeMessage("Guillaume");
+  var alex = welcomeMessage("Alex");
+  var fred = welcomeMessage("Fred");
+    guillaume();
+  alex();
+  fred();
